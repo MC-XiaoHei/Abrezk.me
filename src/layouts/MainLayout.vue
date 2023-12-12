c583b2b2d9ae959c215301281d429673b2a0281d
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-12-12 21:47:34 +0800

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
