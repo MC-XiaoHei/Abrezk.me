2a183a0ac096da96f6bb8b4c0cf23ce85b8c24e8
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-12-13 21:25:05 +0800

    feat: add adaptive layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,11 @@
 <template>
   <q-page class="row items-center justify-evenly">
-
+    <div class="row items-center justify-evenly" style="width: 700px;background-color: lightblue;min-height: 400px">
+      <div class="col-12 col-md-6" style="background-color: aliceblue;height: 400px;width: 350px">
+      </div>
+      <div class="col-12 col-md-6" style="background-color: aliceblue;height: 400px;width: 350px">
+      </div>
+    </div>
     <a class="text-white full-width text-center">Author: MC_XiaoHei</a>
   </q-page>
 </template>
