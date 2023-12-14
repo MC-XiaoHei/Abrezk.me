aff2eeffc4284848c0867b5ada2ef38add60a06a
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-12-14 21:56:07 +0800

    feat: finish layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,35 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <div class="row items-center justify-evenly" style="width: 700px;background-color: lightblue;min-height: 400px">
-      <div class="col-12 col-md-6" style="background-color: aliceblue;height: 400px;width: 350px">
+  <q-page class="row flex-center">
+    <div class="col-12 row flex-center">
+      <div class="col-12 col-md-6 row" style="height: 300px;width: 350px">
+        <div class="col-5 full-height" style="background-color: red">
+          <div style="background-color: green;height: 220px">
+          </div>
+          <div style="background-color: yellow;height: 80px">
+          </div>
+        </div>
+        <div class="col-7 full-height" style="background-color: blue">
+          <div style="background-color: lightgreen;height: 160px">
+          </div>
+          <div style="background-color: lightyellow;height: 140px">
+          </div>
+        </div>
       </div>
-      <div class="col-12 col-md-6" style="background-color: aliceblue;height: 400px;width: 350px">
+      <div class="col-12 col-md-6 reverse-md row" style="height: 300px;width: 350px">
+        <div class="col-5 full-height" style="background-color: red">
+        </div>
+        <div class="col-7 full-height" style="background-color: blue">
+          <div style="background-color: green;height: 200px">
+          </div>
+          <div style="background-color: yellow;height: 100px">
+          </div>
+        </div>
       </div>
-    </div>
-    <div>
-      <a>Author: </a><a style="text-decoration: none;color: #fff" href="https://github.com/MC-XiaoHei">MC_XiaoHei</a><a> Src: </a><a
-      style="text-decoration: none;color: #fff" href="https://github.com/MC-XiaoHei/Abrezk.me">GitHub</a><a> Opensource below The MIT License.</a>
+      <div class="col-12 text-center">
+        <a>Author: </a><a style="text-decoration: none;color: #fff"
+                          href="https://github.com/MC-XiaoHei">MC_XiaoHei</a><a> Src: </a><a
+        style="text-decoration: none;color: #fff" href="https://github.com/MC-XiaoHei/Abrezk.me">GitHub</a>
+      </div>
     </div>
   </q-page>
 </template>
