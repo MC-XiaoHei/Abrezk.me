e6ade1fcee592772006c9ea65dd72289d5e25283
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-12-15 11:39:04 +0800

    fix: fix wrap bug

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,14 @@
 <template>
   <q-page class="row flex-center">
-    <div class="col-12 row flex-center">
+    <div class="flex-center row main-div">
       <div class="col-12 col-md-6 row" style="height: 300px;width: 350px">
-        <div class="col-5 full-height" style="background-color: red">
+        <div class="col-5 full-height">
           <div style="background-color: green;height: 220px">
           </div>
           <div style="background-color: yellow;height: 80px">
           </div>
         </div>
-        <div class="col-7 full-height" style="background-color: blue">
+        <div class="col-7 full-height">
           <div style="background-color: lightgreen;height: 160px">
           </div>
           <div style="background-color: lightyellow;height: 140px">
@@ -18,7 +18,7 @@
       <div class="col-12 col-md-6 reverse-md row" style="height: 300px;width: 350px">
         <div class="col-5 full-height" style="background-color: red">
         </div>
-        <div class="col-7 full-height" style="background-color: blue">
+        <div class="col-7 full-height">
           <div style="background-color: green;height: 200px">
           </div>
           <div style="background-color: yellow;height: 100px">
@@ -37,3 +37,9 @@
 <script setup lang="ts">
 
 </script>
+
+<style lang="sass">
+.main-div
+  @media (max-width: 1024px)
+    width: 350px
+</style>
