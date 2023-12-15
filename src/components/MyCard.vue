435a37237b3737e10a7113a24930c29713afc7f0
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-12-15 16:22:48 +0800

    feat: add MyCard

---
+++ src/components/MyCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <q-card class="full-width full-height">
+    <slot/>
+  </q-card>
+</template>
+
+<style scoped lang="sass">
+
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,25 +3,39 @@
     <div class="flex-center row main-div">
       <div class="col-12 col-md-6 row" style="height: 300px;width: 350px">
         <div class="col-5 full-height">
-          <div style="background-color: green;height: 220px">
+          <div style="height: 220px">
+            <my-card>
+            </my-card>
           </div>
-          <div style="background-color: yellow;height: 80px">
+          <div style="height: 80px">
+            <my-card>
+            </my-card>
           </div>
         </div>
         <div class="col-7 full-height">
-          <div style="background-color: lightgreen;height: 160px">
+          <div style="height: 160px">
+            <my-card>
+            </my-card>
           </div>
-          <div style="background-color: lightyellow;height: 140px">
+          <div style="height: 140px">
+            <my-card>
+            </my-card>
           </div>
         </div>
       </div>
       <div class="col-12 col-md-6 reverse-md row" style="height: 300px;width: 350px">
-        <div class="col-5 full-height" style="background-color: red">
+        <div class="col-5 full-height">
+          <my-card>
+          </my-card>
         </div>
         <div class="col-7 full-height">
-          <div style="background-color: green;height: 200px">
+          <div style="height: 200px">
+            <my-card>
+            </my-card>
           </div>
-          <div style="background-color: yellow;height: 100px">
+          <div style="height: 100px">
+            <my-card>
+            </my-card>
           </div>
         </div>
       </div>
@@ -36,6 +50,7 @@
 
 <script setup lang="ts">
 
+import MyCard from "components/MyCard.vue";
 </script>
 
 <style lang="sass">
