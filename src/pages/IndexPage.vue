c28132268fdc507a1f9600035a8e2078bc5dd45e
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-12-16 07:23:26 +0800

    refactor: refactor code

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,9 @@
 <template>
-
   <q-page class="row flex-center">
-    <q-img no-native-menu no-transition fetchpriority="high" class="fit" fit="cover" src="/background.jpg"
+    <q-img no-native-menu no-transition fetchpriority="high" class="fit" fit="cover" src="/images/background.jpg"
            style="z-index: -1;position: absolute"/>
     <div class="flex-center row main-div q-gutter-y-md q-gutter-x-xs">
-      <a class="col-12 text-center text-h6">不是所有破碎的事情都值得修补。</a>
+      <a class="col-12 text-center text-h6">{{ personalized_signature }}</a>
       <div class="col-6 row q-col-gutter-x-md q-pr-md-sm" style="height: 300px;width: 350px">
         <div class="col-5 full-height">
           <div style="height: 220px">
@@ -51,8 +50,8 @@
 </template>
 
 <script setup lang="ts">
-
 import MyCard from 'components/MyCard.vue';
+import personalized_signature from '../texts/personalized_signature';
 </script>
 
 <style lang="sass">
---
+++ src/texts/personalized_signature.ts
@@ -0,0 +1,2 @@
+const personalized_signature = '不是所有破碎的事情都值得修补。';
+export default personalized_signature;
