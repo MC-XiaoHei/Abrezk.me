c17a8f68cc51a341777f72a374518601954da61b
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2024-01-01 01:00:09 +0800

    fix: fix font

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,8 +2,8 @@
   <q-page class="row flex-center">
     <q-img no-native-menu no-transition fetchpriority="high" class="fit" fit="cover" src="/images/background.jpg"
            style="z-index: -1;position: absolute"/>
-    <div class="flex-center row main-div q-gutter-y-md q-gutter-x-xs">
-      <a class="col-12 text-center text-h6">{{ personalized_signature }}</a>
+    <div class="flex-center row main-div q-gutter-y-md q-gutter-x-xs" style="max-width: 720px">
+      <a class="col-9 text-center text-h6">{{ personalized_signature }}</a>
       <div class="col-6 row q-col-gutter-x-md q-pr-md-sm" style="height: 300px;width: 350px">
         <div class="col-5 full-height column">
           <div>
